c66769eb496def30e38bf70bb1425768a590fed1
Ken Trinh <ktrinh.particle@gmail.com>
2024-04-30 11:27:42 -0400

    added hidden menu functionality

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import { useState } from 'react';
 
@@ -5,10 +6,19 @@ import {Button} from "@nextui-org/react";
 
 const Navbar = () => {
 
+  // Add interative hamburger menu on mobile
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
 
   return (
     <div className="container mx-auto px-4 sm:px-8 lg:px-12 xl:px-24">
       <div className="flex flex-wrap items-center md:justify-center mt-2 md:mt-5 py-2 px-10">
+        <div className="flex-1 flex justify-between items-center">
         <ul id="menu" className="hidden md:flex flex-wrap lg:gap-6 justify-center">
           <li>
             <Link href="/travel">
@@ -32,7 +42,7 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
-        <ul id="menu" className="md:flex flex-wrap gap-20 justify-center">
+        <ul id="menu" className="md:flex flex-wrap gap-10 justify-center">
           <li>
             <Link href="/">
               <p className="px-12 py-2 text-lg font-large text-gray-600 dark:text-gray-400 hover:text-blue-500">
@@ -62,6 +72,32 @@ const Navbar = () => {
               </Button>  
             </li>
         </ul>
+          <div className="md:hidden" onClick={toggleMenu}>
+            <svg
+              className="w-6 h-6 text-gray-600 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMenuOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </div> 
+        </div>
       </div>
     </div>
   );
